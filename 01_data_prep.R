ccee8fbec5852a93fa9af55f936e148069337450
Paul Ervin <paervin@gmail.com>
2018-11-21 07:37:34 -0500

    added some data cleaning examples

--- 01_data_prep.R
+++ 01_data_prep.R
@@ -10,12 +10,26 @@ pop <-
 #check your work
 head(pop$years_scho)
 
-#create dummy
+#create hhid and a dummy for female
 pop <- 
   pop %>% 
-  mutate(female = as.numeric(sex == 6))
+  mutate(hhid = paste(UPM, NVIVI, NHOGA, sep = "_"),
+         female = as.numeric(sex == 6))
 
 #change variable type
 pop <- 
   pop %>% 
   mutate(years_scho = as.integer(years_scho))
+
+#select variables
+pop <- 
+  pop %>% 
+  select(c("hhid", "age", "female", "years_scho", "language"))
+
+#select sample of observations
+pop <- 
+  pop %>% 
+  filter(age >= 8 & age <=17)
+
+#check work
+table(pop$age)
